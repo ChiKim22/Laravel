6fdd59434a915fb8170621ca4016ee36f2aa5ca9
ChiKim22 <wlsgh2505@gmail.com>
2021-07-01 15:16:12 +0900

    Laravel

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
-{
+{   //모델은 하나의 레코드
+    protected $table = 'posts';
     use HasFactory;
 }
