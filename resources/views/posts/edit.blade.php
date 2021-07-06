0515e638e344583a68bb5a043766329de6cdf34c
ChiKim22 <wlsgh2505@gmail.com>
2021-07-06 16:49:04 +0900

    Post +edit function

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,59 @@
+  <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Posts</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script></head>
+    </head>
+    <body>
+        
+        <form action="{{ route('post.update', ['id'=>$post->id]) }}"
+            method="post"
+            enctype="multipart/form-data">
+
+                    @csrf
+                    @method("put")
+
+                    <div class="container mt-3">
+                        <label for="title" >제목</label>
+                        <input type="text" name="title" class="form-control" id="title" placeholder="Title" 
+                        value="{{ old('title') ? old('title') : $post->title }} ">
+
+                            @error('title')
+                                <div>{{ $message }}</div>
+                            @enderror
+
+                      </div>
+                      
+                      <div class="container mt-3">
+                        <label for="content" class="form-label">내용</label>
+                        <textarea class="form-control" name="content" id="content" 
+                        rows="15">{{ old('content') ? old('content') : $post->content }}</textarea>
+
+                        @error('content')
+                                <div>{{ $message }}</div>
+                            @enderror
+                      </div>
+
+                      <div class="container mt-3">
+                        <img src="{{ $post->imagePath() }}" width="20%" class="img-thumnail">
+                      </div>
+
+                      <div class="container mt-3">
+                          <label for="file">File</label>
+                          <input type="file" id="file" name="imageFile">
+    
+                          @error('imageFile')
+                           <div>{{ $message }}</div>
+                          @enderror
+
+                      </div>
+                      <div class="submitbtn">
+                        <button type="submit" class="btn btn-outline-primary">등록하기</button>
+                      </div>
+                </form>
+                </div>        
+    </body>
+    </html>
