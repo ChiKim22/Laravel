eb8315225f8c1ebbc4c1700260120efbcb052416
ChiKim22 <wlsgh2505@gmail.com>
2021-07-08 19:18:51 +0900

    Create (+ CKEditor)

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <title>Posts</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script></head>
+    <script src="https://cdn.ckeditor.com/ckeditor5/29.0.0/classic/ckeditor.js"></script>
 </head>
 <body>
     
@@ -27,16 +27,16 @@
             {{-- <form action="/posts/store" method="post">  --}}
                 @csrf
                 <legend style="text-align: center">글쓰기</legend>
-                <div class="mb-3">
+                <div class="container mb-3">
                     <label for="title" class="form-label">제목</label>
                     <input type="text" name="title" class="form-control" id="title" placeholder="Title" value="{{ old('title') }}">
                         @error('title')
                             <div>{{ $message }}</div>
                         @enderror
                   </div>
-                  <div class="mb-3">
+                  <div class="container mb-3">
                     <label for="content" class="form-label">내용</label>
-                    <textarea class="form-control" name="content" id="content" rows="15">{{ old('content') }}</textarea>
+                    <textarea class="form-control" name="content" id="content" rows="5">{{ old('content') }}</textarea>
                     @error('content')
                             <div>{{ $message }}</div>
                         @enderror
@@ -53,7 +53,17 @@
                     <button type="submit" class="btn btn-outline-primary">등록하기</button>
                   </div>
             </form>
-            </div>        
+            </div>   
+            <script>
+                ClassicEditor
+                        .create( document.querySelector( '#content' ) )
+                        .then( editor => {
+                                console.log( editor );
+                        } )
+                        .catch( error => {
+                                console.error( error );
+                        } );
+        </script>
 </body>
 </html>
 </x-app_layout>
