d737d7da90a58e5ca35eb550ff1a79520ebe8639
ChiKim22 <wlsgh2505@gmail.com>
2021-07-08 19:19:09 +0900

    edit (+CKEditor)

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <title>Posts</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    
+        <script src="https://cdn.ckeditor.com/ckeditor5/29.0.0/classic/ckeditor.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script></head>
     </head>
     <body>
@@ -54,6 +54,16 @@
                         <button type="submit" class="btn btn-outline-primary">등록하기</button>
                       </div>
                 </form>
-                </div>        
+                </div>
+                <script>
+                  ClassicEditor
+                          .create( document.querySelector( '#content' ) )
+                          .then( editor => {
+                                  console.log( editor );
+                          } )
+                          .catch( error => {
+                                  console.error( error );
+                          } );
+          </script>        
     </body>
     </html>
