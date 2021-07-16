224ef354608a0aa37e0b5ca0a31e7a9e32c2c0ba
ChiKim22 <wlsgh2505@gmail.com>
2021-07-16 20:04:42 +0900

    Google login

--- app/Http/Controllers/GoogleAuthController.php
+++ app/Http/Controllers/GoogleAuthController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Str;
 
 class GoogleAuthController extends Controller
 {
@@ -17,6 +21,17 @@ class GoogleAuthController extends Controller
     }
 
     public function callback() {
+
+        $user = Socialite::driver('google')->user();
+        // dd($user);
+
+        $user = User::firstOrCreate([ // 이미 있으면 걸러줌.
+            'email' => $user->getEmail()], // 검색조건
+            ['password' => Hash::make(Str::random(24)),
+            'name' => $user->getName()]
+        );
+        Auth::login($user);
         
+        return redirect()->intended('/dashboard');
     }
 }
