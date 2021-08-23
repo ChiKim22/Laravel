3783e56784a68d6dc42606821423aeeb1ecc7fc2
ChiKim22 <wlsgh2505@gmail.com>
2021-08-23 20:20:06 +0900

    Laravel + Kakao Sign in

--- app/Http/Controllers/KakaoAuthController.php
+++ app/Http/Controllers/KakaoAuthController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Algolia\AlgoliaSearch\Http\Psr7\Request;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Laravel\Socialite\Facades\Socialite;
@@ -11,6 +11,8 @@ use Illuminate\Support\Str;
 
 class KakaoAuthController extends Controller
 {
+    // protected $fillable = ['name', 'email', 'password', 'updated_at', 'created_at'];
+
     public function __construct() {
         $this->middleware(['guest']);
     }
@@ -22,12 +24,13 @@ class KakaoAuthController extends Controller
     public function callback() {
         $user = Socialite::driver('kakao')->user();
 
-        dd($user);
-        $user = User::firstOrCreate([
-            'email' => $user->getEmail()],
+        // dd($user);
+
+        $user = User::firstOrCreate(['email' => $user->getEmail()],
             ['password' => Hash::make(Str::random(24)),
             'name' => $user->getName()]
         );
+   
         Auth::login($user);
 
         return redirect()->intended('/dashboard');
